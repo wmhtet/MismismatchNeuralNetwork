5d71cb98fbcdc3533a054766fe91c41de3f8e549
Win Myo Htet <whtet@directv.com>
2016-06-13 17:09:49 -0700

    Initial Commit

---
+++ MismismatchNeuralNetwork/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MismismatchNeuralNetwork
+//
+//  Created by Win Myo Htet on 6/13/16.
+//  Copyright © 2016 Win Myo Htet. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
